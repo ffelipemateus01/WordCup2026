8f8c0db85e577ffa9dbbac8e2a885398198b9460
Felipe Mateus <fmfoliveira.dev@gmail.com>
2026-06-20 14:12:24 -0300

    First commit

---
+++ src/game.py
@@ -0,0 +1,11 @@
+import pygame
+from src.constants.window import WIN_WIDTH, WIN_HEIGHT
+from src.menu import Menu
+
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        '''Looping principal do game'''
+        pass
